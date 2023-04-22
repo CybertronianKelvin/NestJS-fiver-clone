797d1606807bdfd4d01a15506558d389a6d62d56
kelvinatawurausay <kelvin.atawura@theteachercloud.net>
2023-04-22 20:58:13 +0100

    - Updated Gig DTOs

--- src/gigs/dto/create-gig.dto.ts
+++ src/gigs/dto/create-gig.dto.ts
@@ -1,1 +1,30 @@
-export class CreateGigDto {}
+import { IsNotEmpty, IsOptional, IsString, IsEnum, IsNumber, ArrayNotEmpty } from 'class-validator';
+export class CreateGigDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    category: string;
+  
+    @IsNotEmpty()
+    sellerId: string;
+  
+    @IsNotEmpty()
+    @IsNumber()
+    price: number;
+  
+    @IsOptional()
+    @IsEnum(['Active', 'Inactive'])
+    status?: string;
+  
+    @IsOptional()
+    @ArrayNotEmpty()
+    @IsString({ each: true })
+    tags?: string[];
+  }
--- src/gigs/dto/update-gig.dto.ts
+++ src/gigs/dto/update-gig.dto.ts
@@ -1,4 +1,35 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsEnum, IsArray, ArrayNotEmpty, IsString, IsNumber } from 'class-validator';
 import { CreateGigDto } from './create-gig.dto';
+import { Gig } from '../interfaces/gig.interface';
 
-export class UpdateGigDto extends PartialType(CreateGigDto) {}
+export class UpdateGigDto extends PartialType(CreateGigDto) {
+  @IsOptional()
+  @IsString()
+  title?: Gig['title'];
+
+  @IsOptional()
+  @IsString()
+  description?: Gig['description'];
+
+  @IsOptional()
+  @IsString()
+  category?: Gig['category'];
+
+  @IsOptional()
+  sellerId?: Gig['sellerId'];
+
+  @IsOptional()
+  @IsNumber()
+  price?: Gig['price'];
+
+  @IsOptional()
+  @IsEnum(['Active', 'Inactive'])
+  status?: Gig['status'];
+
+  @IsOptional()
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  tags?: Gig['tags'];
+}
