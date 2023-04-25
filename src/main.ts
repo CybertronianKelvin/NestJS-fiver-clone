1441ca2dda33e0abde2be6a4da0726f170902556
kelvinatawurausay <kelvin.atawura@theteachercloud.net>
2023-04-25 02:08:46 +0100

    - API prefix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Fiverr Clone API')
     .setDescription('Possible endpoints for Fiverr Clone')
